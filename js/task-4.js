e1a72acb3fad9b6e2621c98b318815b672cea251
Zakhar <zaharlazarev123@gmail.com>
2024-06-06 19:22:31 +0300

    update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,27 +1,21 @@
-const getTotalBalanceByGender = (users, gender) => {
-  const arrayM = [];
-  const arrayF = [];
-
-  users.forEach(user => {
-    if (user.gender === 'male') {
-      arrayM.push(user.balance);
-    } else if (user.gender === 'female') {
-      arrayF.push(user.balance);
-    }
-  });
-
-  const resultMale = arrayM.reduce((previous, now) => previous + now, 0);
-  const resultFemale = arrayF.reduce((previousF, nowF) => previousF + nowF, 0);
-
-  if (gender === "male") {
-    return resultMale;
-  } else if (gender === "female") {
-    return resultFemale;
+const getTotalBalanceByGender = ((users, gender)=>{
+  const arrayM=users.filter(user=>user.gender==="male")
+  .reduce((a,b)=>{
+    return a+b.balance;
+  },0)
+  const arrayF=users.filter(user=>user.gender==="female")
+  .reduce((c,d)=>{
+    return c+d.balance;
+  },0)
+  if(gender==="male"){
+    return arrayM;
   }
-}
-
+  else{
+    return arrayF;
+  }
+})
 const clients = [
-  {
+	{
     name: "Moore Hensley",
     gender: "male",
     balance: 2811
@@ -58,5 +52,6 @@ const clients = [
   }
 ];
 
-console.log(getTotalBalanceByGender(clients, "male"));   // 12053
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+
 console.log(getTotalBalanceByGender(clients, "female")); // 8863
